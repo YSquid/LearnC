cbbabbc5a89b17982e6d14ef323336bea411beae
AhmadKariem <102336493+YSquid@users.noreply.github.com>
2024-10-15 18:39:20 -0600

    numbers and comments

---
+++ numbers/numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    //comment
+
+    /*
+    multi
+    line
+    comment
+    */
+
+    int num = 5;
+    double decimal = 5.75;
+
+    printf("My product is %f ", num * decimal);
+
+    printf("My power result is %f ", pow(num, decimal));
+
+    
+
+
+    return 0;
+}
