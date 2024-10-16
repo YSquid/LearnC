7602bcc0c2f34b9fb141181b1baafe8588dcf796
AhmadKariem <102336493+YSquid@users.noreply.github.com>
2024-10-15 18:02:08 -0600

    intial

---
+++ helloworld/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    char food[] = "soup";
+    
+    int number = 8;
+
+
+    printf("I like %s and the number %d\n", food, number);
+    printf("John likes cheese and the number 7\n");
+    printf("Martha hates cheese and the number 7\n");
+    return 0;
+}
---
+++ types/types.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    int age = 40;
+    double price  = 2.5;
+    char grade = 'A';
+    char phrase[] = "Lets go";
+
+    return 0;
+}
